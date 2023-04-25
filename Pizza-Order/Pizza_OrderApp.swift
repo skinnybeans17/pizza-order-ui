23cd73fd733096112b4f2c05e6bf7708d9761a51
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-04-25 13:39:53 -0700

    Initial Commit

---
+++ Pizza-Order/Pizza_OrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_OrderApp.swift
+//  Pizza-Order
+//
+//  Created by mo aabas on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_OrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
