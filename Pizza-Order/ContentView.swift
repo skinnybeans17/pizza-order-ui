fa4c0a9d672bf821ffcd83113d6dd549decfa10a
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2023-04-25 14:10:40 -0700

    12

--- Pizza-Order/ContentView.swift
+++ Pizza-Order/ContentView.swift
@@ -8,19 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    enum PizzaSize: String, CaseIterable, Identifiable {
+        case eight, twelve, twenty
+        var id: Self { self }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    @State var selectedSize: PizzaSize = .twelve
+    @State var inputName = ""
+    @State var inputAddress = ""
+    @State var isVegetarian = false
+    @State var forPickup = false
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Your Name & Address")) {
+                TextField("Name", text: $inputName)
+                TextField("Address", text: $inputAddress)
+            }
+            Section(header: Text("Delivery or Pickup?")) {
+                Toggle("For Pickup", isOn: $forPickup)
+            }
+            Section(header: Text("Size of Pizza & If Vegetarian")) {
+                Picker("Pizza Size", selection: $selectedSize, content: {
+                    Text("8\"").tag(PizzaSize.eight)
+                    Text("12\"").tag(PizzaSize.twelve)
+                    Text("20\"").tag(PizzaSize.twenty)
+                })
+                .pickerStyle(SegmentedPickerStyle())
+                Toggle("Vegetarian", isOn: $isVegetarian)
+            }
+            Section(header: Text("Summary of Order")) {
+                Text("\(inputName) Ordered")
+                Text("\(selectedSize.rawValue) Inch Pizza")
+                Text("Is Vegetarian \(isVegetarian ? "Yes" : "No")")
+            }
+            Section {
+                Button("Place Your Order") {
+                    
+                }
+            }
+        }
     }
 }
